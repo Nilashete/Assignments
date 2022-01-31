7307366bf990f604a695d779889dffa2eefd23ee
Nilashete <91959457+Nilashete@users.noreply.github.com>
2022-01-31 11:23:08 +0530

    Add files via upload

---
+++ typeCasting.java
@@ -0,0 +1,30 @@
+package typecasting;
+
+public class typeCasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Type casting");
+		int a='A';
+		System.out.println("value of a: "+a);
+		
+		int b=a;
+		System.out.println("value of b: "+b);
+		
+		float c=a;
+		System.out.println("value of c: "+c);
+		
+		double e=a;
+		System.out.println("value of e: "+e);
+		
+		System.out.println("\n");
+		
+		System.out.println("Explicit type catsing");
+		
+		double m=20.4;
+		int n=(int)m;
+		System.out.println("value of x: "+m);
+		System.out.println("value of y: "+n);
+	}
+
+}
