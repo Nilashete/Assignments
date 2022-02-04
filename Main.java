e54d1c51710ce0d15ce2e8268f1ec6d9e65ae63c
Nilashete <91959457+Nilashete@users.noreply.github.com>
2022-02-04 23:19:25 +0530

    Order staticstics

--- Main.java
+++ Main.java
@@ -1,14 +1,10 @@
-package com.simplilearn.arrayrotation;
+package com.simplilearn.orderstatictics;
 
 public class Main {
 	public static void main(String[] args) {
-		RotateArray r = new RotateArray();
-        		int arr[] = { 1, 2, 3, 4, 5, 6, 7 }; 
-        		r.rotate(arr, 5); 
-        		for(int i=0;i<arr.length;i++)
-        		{
-            			System.out.print(arr[i]+" ");
-        		}
+		kthSmallst ob = new kthSmallst(); 
+        int arr[] = {12, 3, 5, 7, 4, 19, 26}; 
+        int n = arr.length,k = 4; 
+        System.out.println("K'th smallest element is "+ ob.kthSmallst(arr, 0, n-1, k)); 
 	}
-
 }
